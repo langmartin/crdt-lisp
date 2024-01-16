c4c08cefee2e77b451391904c673a4fda0367bb1
Lang Martin <lang.martin@gmail.com>
2024-01-16 14:38:16 -0500

    schema attempt? maybe this kind of thing can work

--- src/main.lisp
+++ src/main.lisp
@@ -5,6 +5,14 @@
   (:use :cl :local-time)
   (:export zero send recv unix-milliseconds make-hlc))
 
+(defpackage crdt-lisp/cluster
+  (:use :cl :local-time :crdt-lisp/hlc)
+  (:export join-cluster))
+
+(defpackage crdt-lisp/schema
+  (:use :cl)
+  (:export messagep to-network to-lisp))
+
 (in-package :crdt-lisp)
 
 ;; blah blah blah.
---
+++ src/schema.lisp
@@ -0,0 +1,26 @@
+(in-package :crdt-lisp/schema)
+
+(defstruct envelope message hulc)
+(defstruct hlc time tick)
+
+(defun messagep (message)
+  (or (envelope-p message)
+      (hlc-p message)
+      (case (car message)
+        (('HELLO) (and (stringp (cadr message)))))))
+
+(defun to-network (message)
+  (prin1-to-string
+   (cond ((envelope-p message)
+          '(ENV (to-network (envelope-hulc message)) (to-network (envelope-message message))))
+         ((hlc-p message)
+          '(HLC (hlc-time message) (hlc-tick message)))
+         (t message))))
+
+(defun to-lisp (binary)
+  (let ((obj (read-from-string binary)))
+    (case (car obj)
+      ((ENV) (make-envelope :hulc (to-lisp (cadr obj)) :message (to-lisp (caddr obj))))
+      ((HLC) (make-hlc :time (cadr obj) :tick (caddr obj)))
+      ((HELLO) obj)
+      (t nil))))
