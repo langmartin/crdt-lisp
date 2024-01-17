e3f5ce3f05dc627a23f1ab950cc38427ec0fc5e4
Lang Martin <lang.martin@gmail.com>
2024-01-17 12:36:07 -0500

    cluster compiles with the zeromq interface no idea if it works

--- src/cluster.lisp
+++ src/cluster.lisp
@@ -1,13 +1,46 @@
+(ql:quickload :zeromq)
+
 (in-package :crdt-lisp/cluster)
 
-(defstruct cluster name listen connect node-id hlc)
+(defstruct cluster name listen connect node-id hlc peer-connections handler)
 
-(defun join-cluster (&key name listen connect)
-  (make-cluster
-   :name name
-   :listen listen
-   :connect connect
-   :hlc (crdt-lisp/hlc:zero)))
+(defun join-cluster (&key name listen connect handler)
+  (-> (make-cluster :name name
+                    :listen listen
+                    :connect connect
+                    :hlc (crdt-lisp/hlc:zero)
+                    :peer-connections '()
+                    :handler handler)
+      (start-server)
+      (connect-bootstrap)))
 
 (defun send (c message)
-  nil)
+  (mapcan (lambda (peer-socket)
+            (zmq:send (cdr peer-socket) message))
+          (cluster-peer-connections c)))
+
+(defun cluster-add-peer (c peer socket)
+  (make-cluster :name (cluster-name c)
+                :listen (cluster-listen c)
+                :connect (cluster-connect c)
+                :hlc (cluster-hlc c)
+                :peer-connections (cons (cons peer socket)
+                                        (cluster-peer-connections c))
+                :handler (cluster-handler c)))
+
+(defun start-server! (c)
+  (zmq:with-context (ctx)
+    (zmq:with-socket (socket ctx 'zmq:rep)
+      (zmq:bind socket (cluster-listen c))
+      (loop
+        (let ((query (make-instance 'zmq:msg)))
+          (zmq:recv socket query)
+          (funcall (cluster-handler c) query))
+        (zmq:send socket (make-instance 'zmq:msg :data "OK"))))))
+
+(defun connect-bootstrap (c)
+  (zmq:with-context (ctx)
+    (zmq:with-socket (socket ctx 'zmq:req)
+      (let ((peer (cluster-connect c)))
+        (zmq:connect socket peer)
+        (cluster-add-peer c peer socket)))))
